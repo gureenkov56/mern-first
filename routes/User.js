469eebb40961936e0d1e1642872edb62471e38dc
gureenkov56 <gureenkov56@gmail.com>
2023-03-05 19:48:55 +0400

    Fix jwt - create by user id

--- routes/User.js
+++ routes/User.js
@@ -65,7 +65,7 @@ async function loginUser(req, res) {
 
     let comparePassword = await bcrypt.compare(req.body.password, findLogin.password)
     if (comparePassword) {
-        const token = jwt.sign({login: req.body.login}, config.get('JWTsecret'))
+        const token = jwt.sign({userid: findLogin._id}, config.get('JWTsecret'))
         return res.status(200).json({ 
             message: 'Authorization is success',
             token,
@@ -87,12 +87,10 @@ function profile(req, res) {
             })
         }
     });
-
 }
 
 function middlewareCheckToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
-    console.log('bearerHeader', bearerHeader);
     if (typeof bearerHeader !== undefined) {
         const bearer = bearerHeader.split(" ");
         const bearerToken = bearer[1];
@@ -102,82 +100,3 @@ function middlewareCheckToken(req, res, next) {
         res.status(403).json({message: 'Only for authed users'})
     }
 }
-
-// const loginAndPasswordValidation = [
-//     check('login')
-//         .isLength({ min: 3 }).withMessage('Login is too short')
-//         .matches('[0-9]').withMessage('Login Must Contain a Number')
-//         .escape(),
-//     check('password')
-//         .matches('[0-9]').withMessage('Password Must Contain a Number'),
-// ]
-
-// async function createUser(req, res) {
-//     const errorsValidate = validationResult(req);
-
-//     console.log('errors: ', errorsValidate.array())
-
-//     // TODO: сделать для каждой ошибки свой вывод
-//     if (!errorsValidate.isEmpty()) {
-//         return res.status(400).json({ errors: errorsValidate.array() })
-//     }
-
-//     try {
-//         let { login, password } = req.body;
-
-//         // проверка есть ли в базе уже такой логин
-//         const checkLogin = await User.findOne({ login });
-//         if (checkLogin) {
-//             return res.status(400).json({ errors: [{ msg: 'Такой login уже существует' }] })
-//         } else {
-//             // здесь где-то ошибка бросается в catch
-
-//             bcrypt.hash(password, 10, function (err, hash) {
-//                 User.insertMany([{ login, password: hash }]);
-//             });
-
-//             console.log('password', password);
-
-//             return res.status(201).json({
-//                 message: `Успех регистрации!`
-//             })
-//         }
-//     } catch (e) {
-//         return res.status(400).json({
-//             e,
-//             msg: 'ошибка регистрации!'
-//         });
-//     }
-
-
-// }
-
-// async function loginFunc(req, res) {
-//     try {
-//         const { login, password } = req.body;
-
-//         const findLogin = await User.findOne({ login });
-//         if (!findLogin) {
-//             return res.status(400).json({ errors: [{ msg: 'Login не зарегистрирован' }] })
-//         }
-
-//         const match = await bcrypt.compare(password, findLogin.password);
-
-//         if (match) {
-//             const token = jwt.sign(
-//                 { userId: findLogin.id },
-//                 config.get('JWTsecret'),
-//                 { expiresIn: '1h' }
-//             );
-
-//             return res.status(200).json({ errors: [{ msg: 'Авторизация успешна' }] })
-//         } else {
-//             return res.status(400).json({ errors: [{ msg: 'Пароль неверный' }] })
-//         }
-
-//     } catch (error) {
-//         console.log('error', error);
-//     }
-
-//     res.send(`Login post end`)
-// }
